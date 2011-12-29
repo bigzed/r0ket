8c5b6258b89807057dfaefe6746231191c23ea6d
jarig <jarig@himbeere.congress.ccc.de>
2011-12-29 04:11:39 -0600

    generate simple matrix

--- firmware/l0dable/labyrinth.c
+++ firmware/l0dable/labyrinth.c
@@ -16,11 +16,22 @@
 #include "usetable.h"
 /**************************************************************************/
 
-#define ORG_NONE 0
-#define ORG_UP 1
-#define ORG_DOWN 2
-#define ORG_LEFT 3
-#define ORG_RIGHT 4
+#define ORI_UP 0
+#define ORI_DOWN 1
+#define ORI_LEFT 2
+#define ORI_RIGHT 3
+#define TYPE_FREE 0
+#define TYPE_PLAYER 1
+#define TYPE_WALL 2
+#define NORMAL 0
+#define LTop 1
+#define RTop 2
+#define LBot 3
+#define RBot 4
+#define LLef 5
+#define RLef 6
+#define LRig 7
+#define RRig 8
 
 bool matrix[24][17];
 
@@ -58,13 +69,48 @@ bool screen_gameover() {
 }
 
 void generate_matrix(bool[24][17]s matrix){
+  for(i=0; i<24; i++){
+    for(j=0; j<17; j++){
+      matrix[i][j] = false;
+    }
+  }
   bool free = true;
-  bool org = ORG_NONE;
-  uint16_t x = getRandom()%24;
-  uint16_t y = get
+  char x = 0;
+  char y = 5;
+  char org = 4; //no origin
+  char direct = 4;
   for(i=0; i<5; i++){
-    while()
-    getRandom()%24;
+    while(free){
+      while((direct==org){
+        direct = getRandom%4;
+      }
+      if(direct==0) y = y-1%17;
+      if(direct==1) x = x+1%24;
+      if(direct==2) y = y+1%17;
+      if(direct==3) y = y+1%24;
+      if(matrix[x][y] == false){
+        matrix[x][y] = true;
+      }
+      else{
+        break;
+      }
+    }
+  }
+}
+
+void draw_matrix(){
+  font = &Font_Labyrinth;
+  lcdFill(0);
+  for(i=0; i<24; i++){
+    for(j=0; j<17; j++){
+      DoString()
+}
+
+static void draw_sprite(char type, char orientation, char x, char y) {
+	font = &Font_Labyrinth;
+	switch(type) {
+		case TYPE_PLAYER:
+	}
 }
 
 
